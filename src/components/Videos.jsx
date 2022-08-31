993a3e7297842434020d45f533a964521e63a646
johnneyop <johnneyop@gmail.com>
2022-08-31 01:28:58 -0500

    Add components
    1. Add ChannelCard, ChannelDetail, Feed, SideBar,VideoCard, fetchFromAPI components

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Stack, Box } from "@mui/material";
 
-const Video = () => {
+import { ChannelCard, Loader, VideoCard } from "./";
+
+const Videos = ({ videos }) => {
+  
   return (
-    <div>
-      Videos
-    </div>
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
-export default Video
+export default Videos;
